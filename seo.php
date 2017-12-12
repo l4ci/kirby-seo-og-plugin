574c4aa10b37df826b561e0ca4ab83d837fc0a33
Volker Otto <volker.otto@digital-masters.de>
2017-12-12 20:40:40 +0100

    added xml sitemap

--- seo.php
+++ seo.php
@@ -15,16 +15,37 @@ $robotsDefault = '
 User-agent: *
 Disallow:';
 
-$robots = c::get('seo.robots.txt', true);
+$robotsEnabled = c::get('seo.robots.txt', true);
 
-if ( $robots !== false){
+if ( $robotsEnabled !== false){
     
-    if ($robots === true) c::set('seo.robots.txt', $robotsDefault);
+    if ($robotsEnabled === true) c::set('seo.robots.txt', $robotsDefault);
 
     $kirby->set('route', array(
         'pattern' => 'robots.txt',
         'action'  => function() {
             return new Response(trim(c::get('seo.robots.txt')), 'text/plain', 200);
+        }
+    ));
+
+    $kirby->set('route', array(
+        'pattern' => 'sitemap',
+        'action'  => function() {
+            $ignore = c::get('seo.xmlsitemap.ignore', array('sitemap', 'error'));
+            header('Content-type: text/xml; charset="utf-8"');
+            echo '<?xml version="1.0" encoding="utf-8"?>';
+            ?>
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                <?php foreach($pages->index() as $p): ?>
+                <?php if(in_array($p->uri(), $ignore)) continue ?>
+                    <url>
+                        <loc><?php echo html($p->url()) ?></loc>
+                        <lastmod><?php echo $p->modified('c') ?></lastmod>
+                        <priority><?php echo ($p->isHomePage()) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
+                    </url>
+                <?php endforeach ?>
+            </urlset>
+            <?php
         }
     ));
 }
